37fc87b1fc8b202f054b57a9e3ecc88db6e440c2
m.mohammadi <m.mohammadi@fwutech.com>
2020-08-24 15:36:30 +0430

    Add images

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-    return("Hello world\n")
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    name = r.get('name').decode()
+    #print(name)
+    return name
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
